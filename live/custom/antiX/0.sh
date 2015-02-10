68141df7dfda42b44d4da61949978b49f479afbe
James Bowlin <bitjam@gmail.com>
2015-02-09 23:05:17 -0700

    Move 1.sh functionality into 0.sh and into /init.
    I ran out of single-digit breakpoints.  This was not an ideal
    solution but it was the best I could come up with ATM.

--- live/custom/antiX/0.sh
+++ live/custom/antiX/0.sh
@@ -1,3 +1,6 @@
+# File: /live/custom/antiX/0.sh
+# antiX Specific /init code
+
 LIST_MODULES=true
 CHECK_BOOTCODES=true
 
@@ -9,3 +12,20 @@ DO_GFX=true
 FANCY_PROMPT="prompt-fancy"
 AUTO_LOGIN_PROG="autologin"
 AUTO_LOGIN_TERMS="2-4"
+
+live_param_filter() {
+    local param
+    for param; do
+        case $param in
+        # Our Live params
+        disable_srv=*|disable_service=*) ;;
+        aX=*|amnt|amnt=*|antiX=*|automount|automount=*|confont=*|conkeys=*);;
+        desktop=*|dpi=*|drvr=*|dummy|fstab=*|hostname=*|kbd=*|kbopt=*|kbvar=*);;
+        lang=*|lean|mean|mirror=*|mount=*|noRox|nodbus|noloadkeys|noprompt);;
+        nosplash|password|password=*|prompt|pw|pw=*|tz=*|ubp=*|ushow=*);;
+        uverb=*|xdrvr=*|xorgconf|xres=*|Xtralean);;
+
+        *) printf "$param " ;;
+        esac
+    done
+}
