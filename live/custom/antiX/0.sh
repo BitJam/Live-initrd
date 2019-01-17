5f43b5737c49e634876dce065be2f1ad1d24aeea
James Bowlin <bitjam@gmail.com>
2019-01-17 03:00:11 -0700

    Remove nofstab option

--- live/custom/antiX/0.sh
+++ live/custom/antiX/0.sh
@@ -44,7 +44,6 @@ live_param_filter() {
         skylakeflicker)  ;;
         i915powersave) ;;
         wicd|nowicd) ;;
-        nofstab) ;;
 
         # Most kernel codes from version 4.10 (plus additions)
         3c574_cs.*=*|3c589_cs.*=*|3c59x.*=*|3w-9xxx.*=*|3w-sas.*=*|8139cp.*=*|8139too.*=*|8250.*=*|8390.*=*);;
